e596b2ed6c5ffb7a43639567189c76a00e4fc2d6
Harpreet Singh <singhsacademics@gmail.com>
2013-12-22 23:40:06 -0500

    HTML validation fix

--- views/v_users_signup.php
+++ views/v_users_signup.php
@@ -1,17 +1,17 @@
 <form method='POST' action='/users/p_signup'>
 
-    <p>First Name : <input type='text' name='first_name' required> <div id='first' class='msg'></div> </p>
+    <p>First Name : <input type='text' name='first_name' required>  </p>
    
 
-   <p> Last Name : <input type='text' name='last_name'  required> <div id='last' class='msg'></div> </p>
+   <p> Last Name : <input type='text' name='last_name'  required>  </p>
    
 
-    <p> Email Id: <input type='email' name='email'  required> <div id='email' class='msg'></div> </p>
+    <p> Email Id: <input type='email' name='email'  required>  </p>
     
 
-    <p> Password : <input type='password' name='password'  required> <div id='pw' class='msg'></div> </p>
+    <p> Password : <input type='password' name='password'  required>  </p>
 	
-	<p> Initial balance : <input type='text' name='balance'  required> <div id='balance' class='msg'></div> </p>
+	<p> Initial balance : <input type='text' name='balance'  required> </p>
   
 	
 	<?php if(isset($error)): ?>
